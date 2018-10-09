7daae8adc9a2b11fe5d49af6e6094f34a53628a4
Rene Ivanov <rene.ivanov@gmail.com>
2018-10-09 10:13:33 +0200

    Change database to PostgreSQL

--- db/migrate/20180817094830_add_user_name_to_people.rb
+++ db/migrate/20180817094830_add_user_name_to_people.rb
@@ -1,6 +1,6 @@
 class AddUserNameToPeople < ActiveRecord::Migration[5.2]
   def change
     add_column :people, :user_name, :string
-    add_column :people, :password, :digest
+    add_column :people, :password, :string
   end
 end
