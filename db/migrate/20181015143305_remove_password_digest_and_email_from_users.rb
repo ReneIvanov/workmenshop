0942870f067f87d1aa0b7d783dde03060b5fefbb
Rene Ivanov <rene.ivanov@gmail.com>
2018-10-15 16:35:55 +0200

    Removed columns password_digest and email form table users AND prepare gem devise. Now continue with import Devise into User model...

---
+++ db/migrate/20181015143305_remove_password_digest_and_email_from_users.rb
@@ -0,0 +1,6 @@
+class RemovePasswordDigestAndEmailFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :password_digest, :string
+    remove_column :users, :email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_132947) do
+ActiveRecord::Schema.define(version: 2018_10_15_143305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,12 +49,10 @@ ActiveRecord::Schema.define(version: 2018_10_15_132947) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "address"
-    t.string "email"
     t.string "telephone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_name"
-    t.string "password_digest"
   end
 
   add_foreign_key "accounts", "users"
