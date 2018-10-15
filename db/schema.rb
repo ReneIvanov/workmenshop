fdb48b053cef3c947f5112b392c2b887dbb965ce
Rene Ivanov <rene.ivanov@gmail.com>
2018-10-15 13:58:09 +0200

    Chanege column name to username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_090949) do
+ActiveRecord::Schema.define(version: 2018_10_15_115042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2018_10_15_090949) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "address"
     t.boolean "workmen"
     t.boolean "customer"
