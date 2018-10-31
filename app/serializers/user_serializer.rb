64b066a08b93a47677a7d234e7d72b2a54b6c70c
Rene Ivanov <rene.ivanov@gmail.com>
2018-10-31 18:51:15 +0100

    Improving JSON serializers - 2

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,18 +3,60 @@ class UserSerializer
   #  @user_obj = user_obj
   #end
 
-  def as_json(users)
-    @users = users
+  def as_json(users_obj)
     @json_hash = {users: []}
 
-    @users.each do |user|
-      user_hash = 
-        {
-          username: user.username,
-          email: user.email
-        }
+    if users_obj.class.name == "ActiveRecord::Relation"
+      build_hash_from_relation(users_obj)
+    elsif users_obj.class.name == "User"
+      build_hash_from_model(users_obj)
+    else
+      return @json_hash
+    end
+  end
+
+  def user_as_json(user)
+    user_hash = 
+      {
+        username: user.username,
+        email: user.email
+      }
+  end
+
+#    @users = users
+#    @json_hash = 
+#
+#    if @users.count > 1
+#      @users.each do |user|
+#        user_hash = 
+#          {
+#            username: user.username,
+#            email: user.email
+#          }
+#        @json_hash[:users] << user_hash
+#      end
+#    else
+#      user_hash = 
+#          {
+#            username: @users.first.username,
+#            email: @users.first.email
+#          }
+#      @json_hash[:users] << user_hash
+#    end 
+#    return @json_hash
+#  end
+#
+  def build_hash_from_relation(users)
+    users.each do |user|
+      user_hash = user_as_json(user)
       @json_hash[:users] << user_hash
     end
     return @json_hash
+  end
+
+  def build_hash_from_model(user)
+    user_hash = user_as_json(user)
+      @json_hash[:users] << user_hash
+      return @json_hash
   end
 end
