b7c259817440ec68be02e9270da7e582ad8534b9
Rene Ivanov <rene.ivanov@gmail.com>
2018-11-05 13:55:50 +0100

    Add mailer configuration into production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+   #Mailer configuration==========================
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+#===================================================
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
