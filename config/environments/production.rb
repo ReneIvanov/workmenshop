749001312e121778bbf294ba1cb75330aed7ba5d
Rene Ivanov <rene.ivanov@gmail.com>
2018-11-06 21:28:54 +0100

    In production.rb server url changed to https://workmenshop.herokuapp.com/

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   #Mailer configuration==========================
-  config.action_mailer.default_url_options = { :host => 'workmenshop.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://workmenshop.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
